48ca4a9607a24d4e84e89bb095867aef4fa96709
Angelo <rideralucar@gmail.com>
2025-06-01 16:08:56 -0500

    Correccion de nombres de variable de Usuario en UsuarioDaoImpltambien en las vistas de UsuarioController

--- sistema-citas-medicas/sistema-citas-medicas/Dao/DaoImpl/UsuarioDaoImpl.cs
+++ sistema-citas-medicas/sistema-citas-medicas/Dao/DaoImpl/UsuarioDaoImpl.cs
@@ -23,14 +23,14 @@ namespace sistema_citas_medicas.Dao.DaoImpl
                     cmd.CommandType = System.Data.CommandType.StoredProcedure;
                     cmd.Parameters.Clear();
                     cmd.Parameters.AddWithValue("@indicador", indicador);
-                    cmd.Parameters.AddWithValue("@id_usuario", objUsario.idUsuario);
-                    cmd.Parameters.AddWithValue("@nombre", objUsario.nombre);
-                    cmd.Parameters.AddWithValue("@apellido", objUsario.apellido);
-                    cmd.Parameters.AddWithValue("@correo", objUsario.correo);
-                    cmd.Parameters.AddWithValue("@contraseña", objUsario.contraseña);
-                    cmd.Parameters.AddWithValue("@telefono", objUsario.telefono);
-                    cmd.Parameters.AddWithValue("@rol", objUsario.rol);
-                    cmd.Parameters.AddWithValue("@foto_perfil", objUsario.fotoPerfil);
+                    cmd.Parameters.AddWithValue("@id_usuario", objUsario.IdUsuario);
+                    cmd.Parameters.AddWithValue("@nombre", objUsario.Nombre);
+                    cmd.Parameters.AddWithValue("@apellido", objUsario.Apellido);
+                    cmd.Parameters.AddWithValue("@correo", objUsario.Correo);
+                    cmd.Parameters.AddWithValue("@contraseña", objUsario.Contraseña);
+                    cmd.Parameters.AddWithValue("@telefono", objUsario.Telefono);
+                    cmd.Parameters.AddWithValue("@rol", objUsario.Rol);
+                    cmd.Parameters.AddWithValue("@foto_perfil", objUsario.FotoPerfil);
                     procesar = cmd.ExecuteNonQuery();
                 }
             }
@@ -53,14 +53,14 @@ namespace sistema_citas_medicas.Dao.DaoImpl
                     cmd.Parameters.Clear();
 
                     cmd.Parameters.AddWithValue("@indicador", indicador);
-                    cmd.Parameters.AddWithValue("@id_usuario", objUsuario.idUsuario);
-                    cmd.Parameters.AddWithValue("@nombre", objUsuario.nombre);
-                    cmd.Parameters.AddWithValue("@apellido", objUsuario.apellido);
-                    cmd.Parameters.AddWithValue("@correo", objUsuario.correo);
-                    cmd.Parameters.AddWithValue("@contraseña", objUsuario.contraseña);
-                    cmd.Parameters.AddWithValue("@telefono", objUsuario.telefono);
-                    cmd.Parameters.AddWithValue("@rol", objUsuario.rol);
-                    cmd.Parameters.AddWithValue("@foto_perfil", objUsuario.fotoPerfil);
+                    cmd.Parameters.AddWithValue("@id_usuario", objUsuario.IdUsuario);
+                    cmd.Parameters.AddWithValue("@nombre", objUsuario.Nombre);
+                    cmd.Parameters.AddWithValue("@apellido", objUsuario.Apellido);
+                    cmd.Parameters.AddWithValue("@correo", objUsuario.Correo);
+                    cmd.Parameters.AddWithValue("@contraseña", objUsuario.Contraseña);
+                    cmd.Parameters.AddWithValue("@telefono", objUsuario.Telefono);
+                    cmd.Parameters.AddWithValue("@rol", objUsuario.Rol);
+                    cmd.Parameters.AddWithValue("@foto_perfil", objUsuario.FotoPerfil);
 
                     using (SqlDataReader reader = cmd.ExecuteReader())
                     {
@@ -68,14 +68,14 @@ namespace sistema_citas_medicas.Dao.DaoImpl
                         {
                             Aeropuertos = new Usuario()
                             {
-                                idUsuario = reader.IsDBNull(0) ? 0 : reader.GetInt32(0),
-                                nombre = reader.IsDBNull(1) ? string.Empty : reader.GetString(1),
-                                apellido = reader.IsDBNull(2) ? string.Empty : reader.GetString(2),
-                                correo = reader.IsDBNull(3) ? string.Empty : reader.GetString(3),
-                                contraseña = reader.IsDBNull(4) ? string.Empty : reader.GetString(4),
-                                telefono = reader.IsDBNull(5) ? string.Empty : reader.GetString(5),
-                                rol = reader.IsDBNull(6) ? string.Empty : reader.GetString(6),
-                                fotoPerfil = reader.IsDBNull(7) ? string.Empty : reader.GetString(7)
+                                IdUsuario = reader.IsDBNull(0) ? 0 : reader.GetInt32(0),
+                                Nombre = reader.IsDBNull(1) ? string.Empty : reader.GetString(1),
+                                Apellido = reader.IsDBNull(2) ? string.Empty : reader.GetString(2),
+                                Correo = reader.IsDBNull(3) ? string.Empty : reader.GetString(3),
+                                Contraseña = reader.IsDBNull(4) ? string.Empty : reader.GetString(4),
+                                Telefono = reader.IsDBNull(5) ? string.Empty : reader.GetString(5),
+                                Rol = reader.IsDBNull(6) ? string.Empty : reader.GetString(6),
+                                FotoPerfil = reader.IsDBNull(7) ? string.Empty : reader.GetString(7)
                             };
                             lista.Add(Aeropuertos);
                         }
