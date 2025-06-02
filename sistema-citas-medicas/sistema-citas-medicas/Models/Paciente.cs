d5f49b0a20081c42cd0dfdb071e1944b18a25e50
jason-vila <jasonvilac@gmail.com>
2025-06-01 21:16:16 -0500

    Agregar atributos DisplayName a todas las clases y sus propiedades

--- sistema-citas-medicas/sistema-citas-medicas/Models/Paciente.cs
+++ sistema-citas-medicas/sistema-citas-medicas/Models/Paciente.cs
@@ -8,11 +8,13 @@ namespace sistema_citas_medicas.Models
 {
     public class Paciente : Usuario
     {
-        
-        [Required]
+        [Display(Name = "Fecha de nacimiento")]
+        [Required(ErrorMessage = "Ingrese una fecha de nacimiento")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime FechaNacimiento { get; set; }
-        [Required]
+
+        [Display(Name = "Grupo sanguíneo")]
+        [Required(ErrorMessage = "Ingrese un grupo sanguíneo")]
         public string GrupoSanguineo { get; set; }
     }
 }
