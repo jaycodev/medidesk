c2b0bb50670fe992487ec6fb4e2cf3c0578e3778
jason-vila <jasonvilac@gmail.com>
2025-06-05 19:55:43 -0500

    Agregar vista de inicio de sesión (sin lógica)

---
+++ sistema-citas-medicas/sistema-citas-medicas/Controllers/CuentaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sistema_citas_medicas.Controllers
+{
+    public class CuentaController : Controller
+    {
+        public ActionResult IniciarSesion()
+        {
+            return View();
+        }
+    }
+}
