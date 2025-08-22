26674e5e922ee850980ad077e46025d8767364bf
Juan <juanjoseblas344@gmail.com>
2025-08-21 22:35:38 -0500

    ♻️ refactor(appointment,notification): move Appointment and Notification models to Domains/Appointment/Models and Domains/Notification/Models

---
+++ Api/Domains/Notification/Models/Notification.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domains.Notification.Models
+{
+    public class Notification
+    {
+        [Display(Name = "Notificacion")]
+        public int NotificationId { get; set; }
+
+        [Display(Name = "Doctor")]
+        public int? DoctorId { get; set; }
+
+        [Display(Name = "Paciente")]
+        public int? PatientId { get; set; }
+
+        [Display(Name = "Id Cita")]
+        public int AppointmentId { get; set; }
+
+        [Display(Name = "Mensaje")]
+        public string Message { get; set; }
+
+        public bool IsRead { get; set; }
+
+        [Display(Name = "Fecha")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedAt { get; set; }
+
+        public string Role { get; set; }
+    }
+}
